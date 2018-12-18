7c49a0357f704b4d276f2d8161b8e83c5da344b1
Check-in Cash <39098753+checkincash@users.noreply.github.com>
2018-12-17 22:49:14 -0200

    new_commit
    lib by new commit

---
+++ public_html/app/_lib/css/Sc4_LigthBlue/Sc4_LigthBlue_tab.php
@@ -0,0 +1,7 @@
+<?php
+$str_button = 'scriptcase5_BlueIcons';
+$index_class_pos = '';
+$index_class_neg = '';
+$index_class_neu = '';
+$str_toolbar_separator = 'scriptcase__NM__Lightblue_separador.gif';
+?>
---
+++ public_html/app/_lib/css/Sc4_Sunny/Sc4_Sunny_tab.php
@@ -0,0 +1,7 @@
+<?php
+$str_button = 'ScriptCase4_Sunny';
+$index_class_pos = '';
+$index_class_neg = '';
+$index_class_neu = '';
+$str_toolbar_separator = 'scriptcase__NM__Scriptcase3Orange_separador.gif';
+?>
